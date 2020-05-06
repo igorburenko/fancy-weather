d58709ba6752a0e9b304d916359704db40ae672f
Ihor Burenko <burenko@gmail.com>
2020-05-06 21:17:05 +0300

    refactor: delete unused code and comments

--- english-for-kids/src/js/index.js
+++ english-for-kids/src/js/index.js
@@ -1,10 +1,5 @@
-// import NavMenu from './nav-menu';
 import { makeCategoryField } from './controller';
-// import { showStatistic,  } from './controller';
 
 window.onload = function () {
-  // showStatistic();
   makeCategoryField();
 };
-
-// TODO: минимальная ширина, при которой приложение отображается корректно – 320 рх
